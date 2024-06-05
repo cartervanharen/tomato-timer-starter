eaed96ce1e0bfa9dda56f9f8100306c14ba79ef9
Carter Min-Kee Vanharen <cartervanharen@meta.com>
2024-06-05 11:34:52 -0700

    fix countdown

--- script.js
+++ script.js
@@ -6,93 +6,82 @@ var istimestarted = false;
 const header = document.getElementById("time-left");
 
 const studyButton = document.getElementById("study-btn");
-studyButton.addEventListener("click", studyTimer);
+studyButton.addEventListener("click", () => studyTimer(studyButton));
 
 const breakButton = document.getElementById("break-btn");
-breakButton.addEventListener("click", breakTimer);
+breakButton.addEventListener("click", () => breakTimer(breakButton));
 
-function breakTimer() {
-  console.log("buttonpressbreak");
-  if ((istimestarted == false) & (ispaused == false)) {    //pressing it for frist time
+function breakTimer(button) {
+  if ((istimestarted === false) & (ispaused === false)) { //pressing it for first time
+    settime = 5000;
 
-    settime = 5000
+    header.textContent = "Break"; //temp!!!!!!!!!!!!!!
 
-    header.textContent = "break"; //temp!!!!!!!!!!!!!!
-
-
-    breakButton.textContent = "Pause";
+    button.textContent = "Pause";
     studyButton.disabled = true;
     istimestarted = true;
     ispaused = false;
     console.log("1");
-  } else if ((istimestarted == true) & (ispaused == false)) {    // pressing pause
+  } else if ((istimestarted === true) & (ispaused === false)) { // pressing pause
 
     studyButton.disabled = false;
-    breakButton.textContent = "Start";
+    button.textContent = "Start";
     ispaused = true;
     console.log("2");
-  } else if ((istimestarted == true) & (ispaused == true)) {      // resuming part way thru time
-
+  } else if ((istimestarted === true) & (ispaused === true)) { // resuming part way thru time
 
     studyButton.disabled = true;
-    breakButton.textContent = "Pause";
+    button.textContent = "Pause";
     ispaused = false;
     console.log("3");
-    studyButton.textContent = "Study"
+        button.textContent = "Break"
   }
 }
 
-function studyTimer() {
-    console.log("buttonpressstudy");
-    if ((istimestarted == false) & (ispaused == false)) {      //pressing it for frist time
+function studyTimer(button) {
+    if ((istimestarted === false) & (ispaused === false)) { //pressing it for first time
 
-      settime = 1500
+      settime = 1500;
       header.textContent = "Study"; //temp!!!!!!!!!!!!!!
 
-
-      studyButton.textContent = "Pause";
+      button.textContent = "Pause";
       breakButton.disabled = true;
       istimestarted = true;
       ispaused = false;
       console.log("1");
-    } else if ((istimestarted == true) & (ispaused == false)) {    // pressing pause
+    } else if ((istimestarted === true) & (ispaused === false)) { // pressing pause
 
 
       breakButton.disabled = false;
-      studyButton.textContent = "Start";
+      button.textContent = "Start";
       ispaused = true;
       console.log("2");
-    } else if ((istimestarted == true) & (ispaused == true)) {      // resuming part way thru time
+    } else if ((istimestarted === true) & (ispaused === true)) { // resuming part way thru time
 
       breakButton.disabled = true;
-      studyButton.textContent = "Pause";
+      button.textContent = "Pause";
       ispaused = false;
       console.log("3");
-      breakButton.textContent = "Break"
+      button.textContent = "Study"
     }
-
 }
 
-var x = 1;
+var x = settime;
 
 setInterval(() => {
 
-    if (ispaused ==false & istimestarted == true)
-        {
-
-            x -= 1;
-            let temp = convertSecondsToTime(x);
-            header.textContent = temp;
-          
-        }
-
-
+    if (ispaused === false && istimestarted === true) {
+        x -= 1;
+        let temp = convertSecondsToTime(x);
+        header.textContent = temp;
+        
+    }
 
 }, 1000);
 
 function convertSecondsToTime(timeinseconds) {
-  let temp = timeinseconds / 60;
-  let mins = Math.floor(temp);
-  let secs = Math.round((temp % 1) * 60);
-  return mins + ":" + ("0" + secs).slice(-2);
+  const temp = timeinseconds / 60;
+  const mins = Math.floor(temp);
+  const secs = Math.round((temp % 1) * 60);
+  return `${mins < 10 ? "0" : ""}${mins}:${secs < 10 ? "0" : ""}${secs}`;
 }
